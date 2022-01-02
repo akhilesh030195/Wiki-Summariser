b4a70d6bc5c177da36afa585b4d98272186d4808
akhilesh030195 <90547274+akhilesh030195@users.noreply.github.com>
2022-01-02 15:25:56 +0530

    Update mongoDBOperations.py

--- mongoDBOperations.py
+++ mongoDBOperations.py
@@ -12,7 +12,7 @@ class MongoDBManagement:
         try:
             self.username = username
             self.password = password
-            self.url = "mongodb+srv://Akhi_01:Akhanu@cluster0.ad6v8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+            self.url = "mongodb+srv://Akhi_XX:AkhXXu@cluster0.ad6v8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
             # self.url = 'localhost:27017'
         except Exception as e:
             raise Exception(f"(__init__): Something went wrong on initiation process\n" + str(e))
